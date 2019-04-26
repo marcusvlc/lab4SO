834e4c86b0e3db5a16bd5cece9675b6be60c36e2
avdLuna <lunaft9@gmail.com>
2019-04-26 09:21:50 -0300

    Configurações iniciais

--- lab_mem/page_replacement/python/phymem.py
+++ lab_mem/page_replacement/python/phymem.py
@@ -12,26 +12,45 @@ class PhysicalMemory:
   """How many bits to use for the Aging algorithm"""
 
   def __init__(self, algorithm):
-    assert algorithm in {"fifo", "nru", "aging", "second-chance"}
+    assert algorithm in {"fifo", "nru", "aging", "second-chance","lru"}
     self.algorithm = algorithm
+    if (algorithm == 'aging'):
+      self.chosenAlgorithm = Aging(self.ALGORITHM_AGING_NBITS)
+    elif (algorithm == 'fifo'):
+      pass
+      #self.chosenAlgorithm = Fifo()
+    elif (algorithm == 'nru'):
+      pass
+      #self.chosenAlgorithm = Nru()
+    elif (algorithm == 'second-chance'):
+      pass
+      #self.chosenAlgorithm = SecondChance()
+    elif (algorithm == 'lru'):
+      pass
+      #self.chosenAlgorithm = Lru()
 
   def put(self, frameId):
     """Allocates this frameId for some page"""
     # Notice that in the physical memory we don't care about the pageId, we only
     # care about the fact we were requested to allocate a certain frameId
-    pass
+    self.chosenAlgorithm.put(frameId)
 
   def evict(self):
     """Deallocates a frame from the physical memory and returns its frameId"""
     # You may assume the physical memory is FULL so we need space!
     # Your code must decide which frame to return, according to the algorithm
-    pass
+    self.chosenAlgorithm.evict()
 
   def clock(self):
     """The amount of time we set for the clock has passed, so this is called"""
     # Clear the reference bits (and/or whatever else you think you must do...)
-    pass
+    self.chosenAlgorithm.clock()
 
   def access(self, frameId, isWrite):
     """A frameId was accessed for read/write (if write, isWrite=True)"""
-    pass
+    self.chosenAlgorithm.access(frameId,isWrite)
+
+class Aging:
+    def __init__(self, nbits):
+      self.pageTable = []
+      self.ALGORITHM_AGING_NBITS = nbits
